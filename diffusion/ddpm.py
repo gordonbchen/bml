bbaff372eea7d26054ed6bb7386ce25f6f556457
gordonbchen <gordonbchen@gmail.com>
2025-11-29 00:29:33 -0500

    * mse loss * smaller min/max alpha * posenc cycle frac hyperparam * scale and shift conv block * correct sampling noise scale

--- diffusion/ddpm.py
+++ diffusion/ddpm.py
@@ -2,11 +2,14 @@ from argparse import ArgumentParser
 from dataclasses import dataclass, asdict
 
 import torch
+import torch.nn.functional as F
 from torch import nn
 from torch.optim import Adam
 from torchvision.datasets import MNIST
 
 from einops import rearrange
+from einops.layers.torch import Rearrange
+
 from torchinfo import summary
 
 import matplotlib.pyplot as plt
@@ -33,7 +36,7 @@ def calc_alpha_bar(t: torch.Tensor, T: int) -> torch.Tensor:
 
 def calc_alpha(alpha_bar: torch.Tensor) -> torch.Tensor:
     alpha = alpha_bar[1:] / alpha_bar[:-1]
-    return alpha.maximum(torch.tensor(0.001, device=alpha_bar.device))
+    return alpha.clip(1e-4, 1 - 1e-4)
 
 
 def add_noise(xs: torch.Tensor, t: torch.Tensor, T: int) -> tuple[torch.Tensor, torch.Tensor]:
@@ -44,13 +47,13 @@ def add_noise(xs: torch.Tensor, t: torch.Tensor, T: int) -> tuple[torch.Tensor, 
 
 
 class PosEnc(nn.Module):
-    def __init__(self, max_pos: int, emb_dim: int):
+    def __init__(self, max_pos: int, emb_dim: int, cycle_frac: float):
         super().__init__()
         self.register_buffer("pos_enc", torch.empty((max_pos, emb_dim), dtype=torch.float32))
         pos = torch.arange(max_pos)
 
-        # Slowest sin/cos makes it 0.25 of a full cycle.
-        base = (max_pos / 0.25) / (2 * torch.pi)
+        # Slowest sin/cos makes it frac of a full cycle.
+        base = (max_pos / cycle_frac) / (2 * torch.pi)
 
         divs = base ** (2 * torch.arange(emb_dim // 2) / emb_dim)
         phase = pos[:, None] / divs
@@ -63,76 +66,77 @@ class PosEnc(nn.Module):
 
 
 class ConvBlock(nn.Module):
-    def __init__(
-        self, in_channels: int, out_channels: int,
-        time_emb_dim: int, group_size: int,
-        max_pool: bool = False, upsample: bool = False
-    ):
+    def __init__(self, dim_in: int, dim_out: int, groups: int):
         super().__init__()
-        assert not (max_pool and upsample), "Cannot max pool and upsample."
+        # TODO: weight standardized conv2d?
+        self.conv = nn.Conv2d(dim_in, dim_out, 3, padding=1, bias=False)
+        self.norm = nn.GroupNorm(groups, dim_out)
 
-        conv_out_channels = out_channels * 2 if upsample else out_channels
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels, conv_out_channels, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.GroupNorm(conv_out_channels // group_size, conv_out_channels),
-            nn.ReLU(),
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(conv_out_channels, conv_out_channels, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.GroupNorm(conv_out_channels // group_size, conv_out_channels),
-            nn.ReLU()
-        )
+    def forward(self, x: torch.Tensor, scale_shift: tuple[torch.Tensor, torch.Tensor] | None = None):
+        x = self.norm(self.conv(x))
+        if not (scale_shift is None):
+            scale, shift = scale_shift
+            x = ((1 + scale) * x) + shift  # TODO: where does scale shift come from?
+        return F.gelu(x)
 
-        self.time_proj = nn.Sequential(
-            nn.Linear(time_emb_dim, time_emb_dim * 4),
-            nn.GELU(),
-            nn.Linear(time_emb_dim * 4, conv_out_channels)
-        )
 
-        if in_channels == conv_out_channels:
-            self.passthrough = nn.Identity()
-        else:
-            self.passthrough = nn.Conv2d(in_channels, conv_out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+class ResBlock(nn.Module):
+    def __init__(self, dim_in: int, dim_out: int, time_dim: int, groups: int, down: bool = False, up: bool = False):
+        super().__init__()
+        conv_dim = dim_out * 2 if up else dim_out
+        self.conv1 = ConvBlock(dim_in, conv_dim, groups)
+        self.conv2 = ConvBlock(conv_dim, conv_dim, groups)
 
-        self.post = nn.Identity()
-        if max_pool:
-            self.post = nn.MaxPool2d(2, 2)
-        elif upsample:
+        self.time_proj = nn.Sequential(nn.GELU(), nn.Linear(time_dim, conv_dim * 2))
+        self.res_conv = nn.Identity() if dim_in == conv_dim else nn.Conv2d(dim_in, conv_dim, 1)
+
+        assert not (down and up), "Cannot max pool and upsample."
+        if down:
             self.post = nn.Sequential(
-                nn.Upsample(scale_factor=2),
-                nn.Conv2d(conv_out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
-                nn.GroupNorm(out_channels // group_size, out_channels),
-                nn.ReLU()
+                Rearrange("b c (h x1) (w x2) -> b (c x1 x2) h w", x1=2, x2=2),
+                nn.Conv2d(conv_dim * 4, dim_out, 3, padding=1)
             )
+        elif up:
+            self.post = nn.Sequential(
+                nn.Upsample(scale_factor=2, mode="nearest"),
+                nn.Conv2d(conv_dim, dim_out, 3, padding=1)
+            )
+        else:
+            self.post = nn.Identity()
 
-    def forward(self, xb: torch.Tensor, time_emb: torch.Tensor):
-        z = self.conv1(xb)
+    def forward(self, x: torch.Tensor, time_emb: torch.Tensor):
         time_emb = rearrange(self.time_proj(time_emb), "b c -> b c 1 1")
-        z = self.conv2(z + time_emb)
-        z = z + self.passthrough(xb)
+        z = self.conv1(x, time_emb.chunk(2, dim=1))
+        z = self.conv2(z)
+        z = z + self.res_conv(x)
         post = self.post(z)
         return (z, post)
 
 
 class UNet(nn.Module):
-    def __init__(self, image_channels: int, conv_channels: int, max_time: int, time_emb_dim: int, group_size: int):
+    def __init__(self, img_dim: int, conv_dim: int, max_time: int, time_dim: int, groups: int, cycle_frac: float):
         super().__init__()
-        self.down_conv1 = ConvBlock(image_channels, conv_channels, time_emb_dim, group_size, max_pool=True)
-        self.down_conv2 = ConvBlock(conv_channels, conv_channels * 2, time_emb_dim, group_size, max_pool=True)
+        self.down_conv1 = ResBlock(img_dim, conv_dim, time_dim, groups, down=True)
+        self.down_conv2 = ResBlock(conv_dim, conv_dim * 2, time_dim, groups, down=True)
 
-        self.bottleneck = ConvBlock(conv_channels * 2, conv_channels * 4, time_emb_dim, group_size)
+        self.bottleneck = ResBlock(conv_dim * 2, conv_dim * 4, time_dim, groups)
 
-        self.up_conv1 = ConvBlock(conv_channels * 4, conv_channels * 2, time_emb_dim, group_size, upsample=True)
-        self.up_conv2 = ConvBlock(conv_channels * 4, conv_channels, time_emb_dim, group_size, upsample=True)
+        self.up_conv1 = ResBlock(conv_dim * 4, conv_dim * 2, time_dim, groups, up=True)
+        self.up_conv2 = ResBlock(conv_dim * 4, conv_dim, time_dim, groups, up=True)
 
-        self.out_conv = ConvBlock(conv_channels * 2, conv_channels * 2, time_emb_dim, group_size)
-        self.to_pix = nn.Conv2d(conv_channels * 2, image_channels, kernel_size=3, stride=1, padding=1, bias=True)
+        self.out_conv = ResBlock(conv_dim * 2, conv_dim * 2, time_dim, groups)
+        self.to_pix = nn.Conv2d(conv_dim * 2, img_dim, 3, padding=1, bias=True)
 
-        self.time_embedding = PosEnc(max_time, time_emb_dim)
+        self.time_emb = nn.Sequential(
+            PosEnc(max_time, time_dim, cycle_frac),
+            nn.Linear(time_dim, time_dim),
+            nn.GELU(),
+            nn.Linear(time_dim, time_dim),
+        )
 
-    def forward(self, xb: torch.Tensor, t: torch.Tensor):
-        time_emb = self.time_embedding(t)
-        res1, down1 = self.down_conv1(xb, time_emb)
+    def forward(self, x: torch.Tensor, t: torch.Tensor):
+        time_emb = self.time_emb(t)
+        res1, down1 = self.down_conv1(x, time_emb)
         res2, down2 = self.down_conv2(down1, time_emb)
         _, z = self.bottleneck(down2, time_emb)
         _, up2 = self.up_conv1(z, time_emb)
@@ -143,24 +147,24 @@ class UNet(nn.Module):
     
 
 @torch.no_grad()
-def sample_diffusion(model: nn.Module, batch_size: int, image_shape: tuple[int, int, int], max_diffusion_steps: int) -> torch.Tensor:
+def sample_diffusion(model: nn.Module, batch_size: int, image_shape: tuple[int, int, int], max_time: int) -> torch.Tensor:
     model.eval()
     batched_shape = (batch_size, *image_shape)
     device = next(model.parameters()).device
     x = torch.randn(batched_shape, dtype=torch.float32, device=device)
 
-    times = torch.arange(max_diffusion_steps + 1, device=device)
-    alpha_bar = calc_alpha_bar(times, max_diffusion_steps)
+    times = torch.arange(max_time + 1, device=device)
+    alpha_bar = calc_alpha_bar(times, max_time)
     alpha = calc_alpha(alpha_bar)
 
-    for t in range(max_diffusion_steps, 0, -1):
+    for t in range(max_time, 0, -1):
         time = times[t].expand(batch_size)
         pred_noise = ((1 - alpha[t-1]) / (1 - alpha_bar[t]).sqrt()) * model(x, time)
         x = (x - pred_noise) / alpha[t-1].sqrt()
 
         if t > 1:
-            # noise_scale = (1 - alpha_bar[t-1]) * (1 - alpha[t-1]) / (1 - alpha_bar[t])
-            noise_scale = 1 - alpha[t-1]
+            noise_scale = (1 - alpha[t-1]) * (1 - alpha_bar[t-1]) / (1 - alpha_bar[t])
+            # noise_scale = 1 - alpha[t-1]
             noise = torch.randn(batched_shape, dtype=torch.float32, device=device) * noise_scale.sqrt()
             x = x + noise
     model.train()
@@ -192,14 +196,17 @@ class CLIParams:
 
 @dataclass
 class Config(CLIParams):
-    conv_channels: int = 32
-    time_emb_dim: int = 32
-    group_size: int = 4
-    max_diffusion_steps: int = 1024
+    conv_dim: int = 32
+    time_dim: int = 32
+    groups: int = 4
+    max_time: int = 512
+    cycle_frac: float = 0.25
 
-    lr: float = 1e-4
-    batch_size: int = 64
-    n_steps: int = 5_001
+    # TODO: lr scheduling? https://www.desmos.com/calculator/1pi7ttmhhb
+    lr: float = 1e-3
+
+    batch_size: int = 128
+    n_steps: int = 16_001
 
     name: str = ""
 
@@ -209,7 +216,7 @@ if __name__ == "__main__":
 
     images = get_mnist()
 
-    model = UNet(images.shape[1], config.conv_channels, config.max_diffusion_steps, config.time_emb_dim, config.group_size)
+    model = UNet(images.shape[1], config.conv_dim, config.max_time, config.time_dim, config.groups, config.cycle_frac)
     print(summary(model, input_data=(images[:config.batch_size], torch.arange(config.batch_size)), depth=1))
     optim = Adam(model.parameters(), lr=config.lr)
 
@@ -220,12 +227,12 @@ if __name__ == "__main__":
     for step in range(config.n_steps):
         xb = images[torch.randint(len(images), (config.batch_size,), device="cuda")]
 
-        t = torch.randint(1, config.max_diffusion_steps + 1, (config.batch_size,), device="cuda")
-        noise, xb_noised = add_noise(xb, t, config.max_diffusion_steps)
+        t = torch.randint(1, config.max_time + 1, (config.batch_size,), device="cuda")
+        noise, xb_noised = add_noise(xb, t, config.max_time)
 
         pred_noise = model(xb_noised, t)
 
-        loss = ((noise - pred_noise) ** 2).sum() / config.batch_size
+        loss = ((noise - pred_noise) ** 2).mean()
         optim.zero_grad()
         loss.backward()
         optim.step()
@@ -235,10 +242,10 @@ if __name__ == "__main__":
             run.log({"loss": loss.item()}, step=step)
             
             if (step % 1000) == 0:
-                samples = sample_diffusion(model, batch_size=16, image_shape=xb[0].shape, max_diffusion_steps=config.max_diffusion_steps).cpu()
+                samples = sample_diffusion(model, batch_size=16, image_shape=xb[0].shape, max_time=config.max_time).cpu()
 
-                density, bins = samples.histogram(bins=50)
-                plt.plot((bins[1:] + bins[:-1]) / 2, density)
+                counts, bins = samples.histogram(bins=50)
+                plt.plot((bins[1:] + bins[:-1]) / 2, counts)
 
                 samples = rearrange(samples , "(row col) c h w -> c (row h) (col w)", row=4, col=4)
                 samples = ((samples.clip(-1, 1) + 1) * (255 / 2)).to(dtype=torch.uint8)
